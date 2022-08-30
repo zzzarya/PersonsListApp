2f40e486ef4c0051f5a5b7eaeec0e69623f087f9
Anton Zarichny <antonzarichny@gmail.com>
2022-08-30 15:33:53 +0300

    Some logic with TableView

--- PersonsListApp/FirstTableViewController.swift
+++ PersonsListApp/FirstTableViewController.swift
@@ -8,38 +8,33 @@
 import UIKit
 
 class FirstTableViewController: UITableViewController {
+    
+    private var personList = Person.getPersonList()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        personList.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "firstPerson", for: indexPath)
+        
+        let person = personList[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(person.name) \(person.surname)"
+        
+        cell.contentConfiguration = content
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
--- PersonsListApp/Person.swift
+++ PersonsListApp/Person.swift
@@ -16,22 +16,25 @@ struct Person {
 
 let personInfo = DataManager()
 
-func getPersonList() -> [Person] {
-    let names = personInfo.names.shuffled()
-    let surnames = personInfo.surnames.shuffled()
-    let numbers = personInfo.numbers.shuffled()
-    let emails = personInfo.emails.shuffled()
-    
-    var persons: [Person] = []
-    
-    for index in 0...9 {
-      let person = Person(name: names[index],
-               surname: surnames[index],
-               number: numbers[index],
-               email: emails[index])
-        persons.append(person)
+extension Person {
+   static func getPersonList() -> [Person] {
+        let names = personInfo.names.shuffled()
+        let surnames = personInfo.surnames.shuffled()
+        let numbers = personInfo.numbers.shuffled()
+        let emails = personInfo.emails.shuffled()
+        
+        var persons: [Person] = []
+        
+        for index in 0...9 {
+            let person = Person(name: names[index],
+                                surname: surnames[index],
+                                number: numbers[index],
+                                email: emails[index])
+            persons.append(person)
+        }
+        
+        return persons
     }
-    
-    return persons
 }
+
 
