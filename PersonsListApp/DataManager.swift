c14995e4b81cfb9388ab8ef3e6799d8e8149c526
Anton Zarichny <antonzarichny@gmail.com>
2022-08-30 15:26:34 +0300

    View controllers and model

---
+++ PersonsListApp/DataManager.swift
@@ -0,0 +1,29 @@
+//
+//  DataManager.swift
+//  PersonsListApp
+//
+//  Created by Антон Заричный on 30.08.2022.
+//
+
+import Foundation
+
+class DataManager {
+    let names = ["Camille", "Hugo", "Ayla", "Martin", "Jason",
+                "Elise", "Jane", "Alessandra", "Zayn", "Hadley"]
+    
+    let surnames = ["Anderson", "Phillips", "Watkins", "Gray", "Gomez",
+                    "Reyes", "Simpson", "Hopkins", "Torres", "Fowler"]
+    
+    let numbers = ["63(276)756-34-86", "629(70)880-00-28", "8(327)849-41-28",
+                   "985(77)827-62-77", "57(015)243-18-44", "335(5373)907-18-56",
+                   "20(363)068-91-26", "61(141)616-32-48", "819(567)640-55-22",
+                   "9(508)648-28-17"]
+    
+    let emails = ["tritchie@yahoo.com", "weissnat.marlee@okon.info",
+                  "ahowell@hotmail.com", "susanna.quitzon@cronin.com",
+                  "travis.gleason@cole.com", "chadd05@yahoo.com",
+                  "seth.wilkinson@gmail.com", "cschaden@yahoo.com",
+                  "louie.gaylord@hotmail.com", "niko.bins@gleason.com"]
+}
+
+
---
+++ PersonsListApp/Person.swift
@@ -0,0 +1,37 @@
+//
+//  Person.swift
+//  PersonsListApp
+//
+//  Created by Антон Заричный on 30.08.2022.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let number: String
+    let email: String
+}
+
+let personInfo = DataManager()
+
+func getPersonList() -> [Person] {
+    let names = personInfo.names.shuffled()
+    let surnames = personInfo.surnames.shuffled()
+    let numbers = personInfo.numbers.shuffled()
+    let emails = personInfo.emails.shuffled()
+    
+    var persons: [Person] = []
+    
+    for index in 0...9 {
+      let person = Person(name: names[index],
+               surname: surnames[index],
+               number: numbers[index],
+               email: emails[index])
+        persons.append(person)
+    }
+    
+    return persons
+}
+
